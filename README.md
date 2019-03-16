b1a174872ae7dd0a4a8c138c88c9a5ac6e013274
Sulman <sulman.malik@hotmail.co.uk>
2019-03-16 20:04:28 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sparta_Global_Calculator_hw
+# Sparta_global_Calculator_hw
---
+++ index.js
@@ -0,0 +1,5 @@
+
+  var num1,num2,ans,operator;
+  num1 = parseInt(prompt("Type a number"));
+  num2 = parseInt(prompt("Type a number"));
+  alert(num1 + num2);
